e27538e4a77664f2d30fdc1a206fbd5cdb8ec98e
7AndreyArt7 <7andron7art@gmail.com>
2025-07-21 18:05:27 +0300

    Menu

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -2,38 +2,46 @@ import styled from "styled-components";
 
 export const Menu = () => {
     return (
-        <StyleMenu>
+        <StyledMenu>
             <ul>
-                <li><a href={"#"}>Home</a></li>
-                <li><a href={"#"}>About</a></li>
-                <li><a href={"#"}>Tech Stack</a></li>
-                <li><a href={"#"}>Projects</a></li>
-                <li><a href={"#"}>Contact</a></li>
+                <StyledLink><a href={"#"}>Home</a></StyledLink>
+                <StyledLink><a href={"#"}>About</a></StyledLink>
+                <StyledLink><a href={"#"}>Tech Stack</a></StyledLink>
+                <StyledLink><a href={"#"}>Projects</a></StyledLink>
+                <StyledLink><a href={"#"}>Contact</a></StyledLink>
             </ul>
-        </StyleMenu>
+        </StyledMenu>
     );
 };
 
-const StyleMenu = styled.nav`
+const StyledMenu = styled.nav`
     max-width: 790px;
     width: 100%;
   
     ul {
-        max-width: 609px;
-        width: 100%;
         display: flex;
-        list-style: none;
         justify-content: space-between;
     }
-
-    a {
-        text-decoration: none;
+    a{
         font-weight: 500;
         font-size: 20px;
         line-height: 1.3;
-        text-align: center;
         color: #a7a7a7;
         font-family: "DM Sans", sans-serif;
+        &:hover {
+            background: linear-gradient(90deg, #13b0f5 2.6%, #e70faa 100%);
+            background-clip: text;
+            -webkit-background-clip: text;
+            -webkit-text-fill-color: transparent;
+        }
+        
+    }
+`
+
+const StyledLink = styled.li`
+    &:hover {
+        transform: scale(1.2);
+        
     }
 `
 
--- src/style/GlobalStyle.tsx
+++ src/style/GlobalStyle.tsx
@@ -3,38 +3,40 @@ import {theme} from "./Theme.tsx";
 
 
 export const GlobalStyle = createGlobalStyle`
-*,
-*:before,
-*:after {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-}
+    *,
+    *:before,
+    *:after {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
 
-body {
-    margin: 0 auto;
-    font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    background-color: ${theme.colors.primary};
-}
+    body {
+        margin: 0 auto;
+        font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+        'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+        sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        background-color: ${theme.colors.primary};
+    }
 
-button {
-    border: none;
-    background-color: unset;
-    cursor: pointer;
-}
-a{
-    text-decoration: none;
-}
+    button {
+        border: none;
+        background-color: unset;
+        cursor: pointer;
+    }
 
-ul{
-    list-style: none;
-}
+    a {
+        text-decoration: none;
+        cursor: pointer;
+    }
 
-@import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+    ul {
+        list-style: none;
+    }
+
+    @import url('https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
 
 
 `
